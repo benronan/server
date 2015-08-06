9407450ce0d231213056da7ad2691dc17d00d69b
benronan <freestylsurfr@cox.net>
2015-08-06 16:25:13 -0500

    commit 2

--- app.js
+++ app.js
@@ -1,80 +1,65 @@
-// var express = require('express');
-// var path = require('path');
-// var async = require('async');
+var express = require('express');
+var path = require('path');
+var async = require('async');
 var email = require('emailjs');
-// var app =  express();
+var app =  express();
 
-// var emailServer  = email.server.connect({
-   // user:"bensacola@gmail.com", 
-   // password: process.env.MAIL_PASS, 
-   // host:"smtp.gmail.com"
-	 // ,port:465
-   // ,ssl:true
-	 // //,port:587
-	 // //,tls:true
-// });
-var email = require('emailjs');
+console.log('process.env.MAIL_USER: ' + process.env.MAIL_USER);
+console.log('process.env.MAIL_PASS: ' + process.env.MAIL_PASS ? 'set' : 'undefined');
 
-var server = email.server.connect({
-  user: 'nodejsiscool@gmail.com',
-  password: 'stackoverflow',
-  host: 'smtp.gmail.com',
-  ssl: true
+
+var emailServer  = email.server.connect({
+	user: process.env.MAIL_USER, 
+	password: process.env.MAIL_PASS, 
+	host:"smtp.cox.com"
+	,port:465
+	,ssl:true
+	//,port:587
+	//,tls:true
 });
 
-server.send({
-  text: 'Hey howdy',
-  from: 'NodeJS',
-  to: 'Wilson <wilson.balderrama@gmail.com>',
-  cc: '',
-  subject: 'Greetings'
-}, function (err, message) {
-  console.log(err || message);
+app.get('/', function(request, response) {
+ response.send("What");
 });
 
+app.get("/ping", function(request, response) {
+ response.json('pong');
+});
 
-// app.get('/', function(request, response) {
-	// response.send("What");
-// });
+app.get('/time', function(request, response) {
+ response.send(new Date());
+});
 
-// app.get("/ping", function(request, response) {
-	// response.json('pong');
-// });
+app.get('/email',function(request, response) {
+	console.log('sending email');
+	send the message and get a callback with an error or details of the message that was sent
+ var message = {
+		text:    "can you hear me now?", 
+		from:    "benronan@cox.net", 
+		to:      "freestylsurfr@cox.net",
+		subject: "testing emailjs"
+ }
+ emailServer.send(message, function(err, message) {
+	 if(err) {
+		 console.log(err); 
+	 }
+	 else {
+		 console.log('send success');
+	 }
+	 });
 
-// app.get('/time', function(request, response) {
-	// response.send(new Date());
-// });
+ console.log('email sending');
+ response.send('email sent');
+});
 
-// app.get('/email',function(request, response) {
-	 // console.log('sending email');
-	// // send the message and get a callback with an error or details of the message that was sent
-	// var message = {
-		 // text:    "i hope this works", 
-		 // from:    "benronan@cox.net", 
-		 // to:      "benronan@cox.net",
-		 // subject: "testing emailjs"
-	// }
-	// emailServer.send(message, function(err, message) {
-		// if(err) {
-			// console.log(err); 
-		// }
-		// else {
-			// console.log('send success');
-		// }
-		// });
-	
-	// console.log('email sending');
-	// response.send('email sent');
-// });
+app.get('/*', function(request, response) {
+ response.redirect('/');
+});
 
-// app.get('/*', function(request, response) {
-	// response.redirect('/');
-// });
-
-// var server = app.listen(4351, function() {
-  // var host = server.address().address;
-	// var port = server.address().port;
-	// console.log('Server listening at http://%s:%s', host, port);
-// });
+var server = app.listen(4351, function() {
+ var host = server.address().address;
+ var port = server.address().port;
+ console.log('Server listening at http://%s:%s', host, port);
+});
 
 
