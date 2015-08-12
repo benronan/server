fe2bc80735ff746a85c5891babca4ea2423c6d9b
benronan <freestylsurfr@cox.net>
2015-08-12 16:47:48 +0000

    pi

--- public_ip.js
+++ public_ip.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 http.get("http://www.google.com/", function(res) {
   console.log("Got response: " + res.statusCode);
-	for(i in res) {
+	for(i in res.client) {
 	  console.log(i + ': ' +  res[i]);
 	}
 }).on('error', function(e) {
