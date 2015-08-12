ac2d583e4d060b8f7e6f39185fe7a7a607b232ca
benronan <freestylsurfr@cox.net>
2015-08-12 16:14:14 +0000

    pi

--- ip.js
+++ ip.js
@@ -5,12 +5,12 @@ var ifaces = os.networkInterfaces();
 
 Object.keys(ifaces).forEach(function (ifname) {
   var alias = 0    ;
-
+console.log('interface: ' + ifname);
   ifaces[ifname].forEach(function (iface) {
-    if ('IPv4' !== iface.family || iface.internal !== false) {
+//    if ('IPv4' !== iface.family || iface.internal !== false) {
       // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
-      return;
-    }
+//      return;
+ //   }
 
     if (alias >= 1) {
       // this single interface has multiple ipv4 addresses
