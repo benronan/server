c7197580116b2119f7d1c01be5c94bf61c30c0e5
benronan <freestylsurfr@cox.net>
2015-08-12 16:39:01 +0000

    PI

--- public_ip.js
+++ public_ip.js
@@ -2,8 +2,8 @@ var http = require('http');
 
 http.get("http://www.google.com/", function(res) {
   console.log("Got response: " + res.statusCode);
-	foreach(var i in res) {
-	  console.log(i + ': " +  res[i]);
+	for(i in res) {
+	  console.log(i + ': ' +  res[i]);
 	}
 }).on('error', function(e) {
   console.log("Got error: " + e.message);
