07776fb74ccfd3c67adb46736825de99bb072335
benronan <freestylsurfr@cox.net>
2015-08-12 11:20:35 -0500

    pc

--- public_ip.js
+++ public_ip.js
@@ -1,18 +1,13 @@
-require('http').request({
-    hostname: 'fugal.net',
-    path: '/ip.cgi',
-    agent: false
-    }, function(res) {
-    if(res.statusCode != 200) {
-        throw new Error('non-OK status: ' + res.statusCode);
-    }
-    res.setEncoding('utf-8');
-    var ipAddress = '';
-    res.on('data', function(chunk) { ipAddress += chunk; });
-    res.on('end', function() {
-			  console.log(ipAddress);
-        // ipAddress contains the external IP address
-    });
-    }).on('error', function(err) {
-    throw err;
-}).end();
+var net = require('net');
+function getNetworkIP(callback) {
+  var socket = net.createConnection(80, 'www.google.com');
+  socket.on('connect', function() {
+    callback(socket.address().address);
+    socket.end();
+  });
+  socket.on('error', function(e) {
+    callback(e, 'error');
+  });
+}
+
+getNetworkIP(function(address){ console.log(address); });
