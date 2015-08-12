8076b73a6648fbaa6b3a77ddd06b176cf84278ba
benronan <freestylsurfr@cox.net>
2015-08-12 12:01:19 -0500

    pc

--- public_ip.js
+++ public_ip.js
@@ -1,12 +1,26 @@
 var http = require('http');
 
-http.get("http://jsonip.com/", function(res) {
+// http.get("http://jsonip.com/", function(res) {
+  // console.log("Got response: " + res.statusCode);
+	// res.on('data', function (chunk) {
+		// var json = JSON.parse(chunk)
+		// var ip = json["ip"];
+		// console.log('ip: ' + ip);
+  // });
+// }).on('error', function(e) {
+  // console.log("Got error: " + e.message);
+// });
+
+http.get("http://ip-api.com/json", function(res) {
   console.log("Got response: " + res.statusCode);
 	res.on('data', function (chunk) {
-    console.log('BODY: ' + chunk);
 		var json = JSON.parse(chunk)
-		var ip = json["ip"];
-		console.log('ip: ' + ip);
+		console.log('ip: ' + json["ip"]);
+		console.log('city: ' + json["city"]);
+		console.log('state: ' + json["regionName"]);
+		console.log('zipcode: ' + json["zip"]);
+		console.log('country: ' + json["ip"]);
+		console.log('coords: ' + json["lat"] + ' , ' + json["lon"]);
   });
 }).on('error', function(e) {
   console.log("Got error: " + e.message);
